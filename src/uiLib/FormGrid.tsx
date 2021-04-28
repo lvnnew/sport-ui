265a3f71e2c2a30d9573817bf9be255a3adea4ea
Damir <tebuchet@gmail.com>
2021-04-28 21:26:02 +0300

    grid

--- src/uiLib/FormGrid.tsx
+++ src/uiLib/FormGrid.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {Children, isValidElement, cloneElement, ReactNode} from 'react';
 import {Record} from 'ra-core';
 import {Grid, GridProps} from '@material-ui/core';
+import { Labeled } from 'react-admin';
+import classnames from 'classnames';
 
 const sanitizeRestProps = ({
   basePath,
@@ -34,12 +36,28 @@ const FormGrid = ({
   <Grid key={version} {...sanitizeRestProps(rest)}>
     {Children.map(children, field =>
       field && isValidElement<any>(field) ? (
-        cloneElement(field, {
-          record,
-          resource,
-          basePath,
-        })
-      ) : null
+        field.props.addLabel ? (
+          <Labeled
+            record={record}
+            resource={resource}
+            basePath={basePath}
+            label={field.props.label}
+            source={field.props.source}
+            disabled={false}
+            fullWidth={field.props.fullWidth}
+          >
+            {field}
+          </Labeled>
+        ) : typeof field.type === 'string' ? (
+          field
+        ) : (
+          cloneElement(field, {
+            record,
+            resource,
+            basePath,
+          })
+        )
+    ) : null
     )}
   </Grid>
 );
