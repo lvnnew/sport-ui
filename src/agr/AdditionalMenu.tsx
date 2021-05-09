7e0e08e83c660d734ae8e411d4cb55699284719a
Damir <tebuchet@gmail.com>
2021-05-09 18:25:57 +0300

    stats

--- src/agr/AdditionalMenu.tsx
+++ src/agr/AdditionalMenu.tsx
@@ -1,7 +1,14 @@
-import * as React from 'react';
-import {
-  FC,
-} from 'react';
+import React, {useState, FC} from 'react';
+import {useSelector} from 'react-redux';
+import {useTranslate, MenuItemLink} from 'react-admin';
+import {AppState} from '../types';
+import orders from '../demo/orders';
+import {useDebug} from '../contexts/DebugContext';
+
+const defaultState = {
+};
+
+type MenuName = keyof typeof defaultState;
 
 export interface AdditionalMenuProps {
   dense: boolean;
@@ -9,9 +16,30 @@ export interface AdditionalMenuProps {
   onMenuClick: () => void;
 }
 
-export const AdditionalMenu: FC<AdditionalMenuProps> = () => {
+export const AdditionalMenu: FC<AdditionalMenuProps> = ({onMenuClick, dense, open}) => {
+  const translate = useTranslate();
+  const [state, setState] = useState(defaultState);
+  useSelector((state: AppState) => state.theme); // force rerender on theme change
+  const {debug} = useDebug();
+
+  const handleToggle = (menu: MenuName) => {
+    setState(state => ({...state, [menu]: !state[menu]}));
+  };
+
   return (
     <>
+      {debug &&
+        <>
+          <MenuItemLink
+            dense={dense}
+            leftIcon={<orders.icon />}
+            onClick={onMenuClick}
+            primaryText={'Stats'}
+            sidebarIsOpen={open}
+            to={'/statsDashboard'}
+          />
+        </>
+      }
     </>
   );
 };
