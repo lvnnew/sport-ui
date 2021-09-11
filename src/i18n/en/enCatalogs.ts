599617e042fd2bf32b2ad9598d74485602718f78
Damir <tebuchet@gmail.com>
2021-09-12 01:28:53 +0300

    gen

---
+++ src/i18n/en/enCatalogs.ts
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+
+// DO NOT EDIT! THIS IS GENERATED FILE
+
+const enCatalogs = {
+  files: 'Files',
+  languages: 'Languages',
+  users: 'Users',
+  appLogins: 'Logins of usual (not admins) users',
+  managers: 'Manager',
+  managerLogins: 'Manager logins',
+  roles: 'Roles',
+  permissions: 'Permissions',
+  rolesToPermissions: 'Roles To Permissions',
+  managersToRoles: 'Managers To Roles',
+  stats: 'Stats',
+  tags: 'Tags',
+};
+
+export default enCatalogs;
---
+++ src/i18n/en/enInfoRegistries.ts
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+
+// DO NOT EDIT! THIS IS GENERATED FILE
+
+const enInfoRegistries = {};
+
+export default enInfoRegistries;
