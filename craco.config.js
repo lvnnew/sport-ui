295a75fc43c6dcef72e815def8aab4e1decb4363
Damir <tebuchet@gmail.com>
2021-11-06 19:16:47 +0300

    gen

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-commonjs */
+const CracoSwcPlugin = require('craco-swc');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoSwcPlugin,
+      options: {
+        swcLoaderOptions: {
+          jsc: {
+            externalHelpers: true,
+            target: 'es5',
+            parser: {
+              syntax: 'typescript',
+              tsx: true,
+              dynamicImport: true,
+            },
+          },
+        },
+      },
+    },
+  ],
+};
