7195ba83a467f090969652cbf98c8c69ae7fc647
Damir <tebuchet@gmail.com>
2021-12-09 17:07:22 +0300

    "deps"

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -23,41 +23,45 @@ export type Scalars = {
   /** A field whose value is a UTC Offset: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones */
   UtcOffset: any;
   /**
-   * A string representing a duration conforming to the ISO8601 standard,
-   * such as: P1W1DT13H23M34S
-   * P is the duration designator (for period) placed at the start of the duration representation.
-   * Y is the year designator that follows the value for the number of years.
-   * M is the month designator that follows the value for the number of months.
-   * W is the week designator that follows the value for the number of weeks.
-   * D is the day designator that follows the value for the number of days.
-   * T is the time designator that precedes the time components of the representation.
-   * H is the hour designator that follows the value for the number of hours.
-   * M is the minute designator that follows the value for the number of minutes.
-   * S is the second designator that follows the value for the number of seconds.
    *
-   * Note the time designator, T, that precedes the time value.
+   *     A string representing a duration conforming to the ISO8601 standard,
+   *     such as: P1W1DT13H23M34S
+   *     P is the duration designator (for period) placed at the start of the duration representation.
+   *     Y is the year designator that follows the value for the number of years.
+   *     M is the month designator that follows the value for the number of months.
+   *     W is the week designator that follows the value for the number of weeks.
+   *     D is the day designator that follows the value for the number of days.
+   *     T is the time designator that precedes the time components of the representation.
+   *     H is the hour designator that follows the value for the number of hours.
+   *     M is the minute designator that follows the value for the number of minutes.
+   *     S is the second designator that follows the value for the number of seconds.
    *
-   * Matches moment.js, Luxon and DateFns implementations
-   * ,/. is valid for decimal places and +/- is a valid prefix
+   *     Note the time designator, T, that precedes the time value.
+   *
+   *     Matches moment.js, Luxon and DateFns implementations
+   *     ,/. is valid for decimal places and +/- is a valid prefix
+   *
    */
   Duration: any;
   /**
-   * A string representing a duration conforming to the ISO8601 standard,
-   * such as: P1W1DT13H23M34S
-   * P is the duration designator (for period) placed at the start of the duration representation.
-   * Y is the year designator that follows the value for the number of years.
-   * M is the month designator that follows the value for the number of months.
-   * W is the week designator that follows the value for the number of weeks.
-   * D is the day designator that follows the value for the number of days.
-   * T is the time designator that precedes the time components of the representation.
-   * H is the hour designator that follows the value for the number of hours.
-   * M is the minute designator that follows the value for the number of minutes.
-   * S is the second designator that follows the value for the number of seconds.
    *
-   * Note the time designator, T, that precedes the time value.
+   *     A string representing a duration conforming to the ISO8601 standard,
+   *     such as: P1W1DT13H23M34S
+   *     P is the duration designator (for period) placed at the start of the duration representation.
+   *     Y is the year designator that follows the value for the number of years.
+   *     M is the month designator that follows the value for the number of months.
+   *     W is the week designator that follows the value for the number of weeks.
+   *     D is the day designator that follows the value for the number of days.
+   *     T is the time designator that precedes the time components of the representation.
+   *     H is the hour designator that follows the value for the number of hours.
+   *     M is the minute designator that follows the value for the number of minutes.
+   *     S is the second designator that follows the value for the number of seconds.
    *
-   * Matches moment.js, Luxon and DateFns implementations
-   * ,/. is valid for decimal places and +/- is a valid prefix
+   *     Note the time designator, T, that precedes the time value.
+   *
+   *     Matches moment.js, Luxon and DateFns implementations
+   *     ,/. is valid for decimal places and +/- is a valid prefix
+   *
    */
   ISO8601Duration: any;
   /** A local date string (i.e., with no associated timezone) in `YYYY-MM-DD` format, e.g. `2020-01-01`. */
