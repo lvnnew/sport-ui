2facc4b123f4f206bfa0a476a08a9aa2bd24ed8d
Damir <tebuchet@gmail.com>
2022-04-03 16:12:36 +0300

    "fix"

--- src/adm/pages/tags/TagShow/index.tsx
+++ src/adm/pages/tags/TagShow/index.tsx
@@ -1,23 +1,12 @@
 /* eslint-disable max-len */
 import React, {FC} from 'react';
 import {
-  Show,
   ShowProps,
-  TabbedShowLayout,
 } from 'react-admin';
-import MainTab from './MainTab';
-import {additionalTabs} from './additionalTabs';
-
-// DO NOT EDIT! THIS IS GENERATED FILE
+import DefaultTagShow from './DefaultTagShow';
 
 const TagShow: FC<ShowProps> = (props: ShowProps) => (
-  <Show {...props}>
-    <TabbedShowLayout>
-      <MainTab label={translate('app.mainTab')} />
-      {additionalTabs.map(({Tab, label}, i) => <Tab label={label} key={i} />)}
-
-    </TabbedShowLayout>
-  </Show>
+  <DefaultTagShow {...props} />
 );
 
 export default TagShow;
