6342edb066882ab64aff1596885549300419d1de
Damir <tebuchet@gmail.com>
2022-04-05 22:26:32 +0300

    "gen"

---
+++ src/uiLib/menu/addLinkToEntityList.tsx
@@ -0,0 +1,10 @@
+import {MenuElement} from './MenuItem';
+
+const addLinkToEntityList = (entityName: string, debugOnly = false) => ({
+  label: `catalogs.${entityName}.title`,
+  link: '/statsDashboard',
+  icon: 'DetailsOutlined',
+  debugOnly,
+} as MenuElement);
+
+export default addLinkToEntityList;
