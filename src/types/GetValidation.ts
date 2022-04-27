1e497a485eda89769ead877f0ebfc67be0b6b8fb
Damir <tebuchet@gmail.com>
2022-04-27 13:01:38 +0300

    'ra4'

--- src/types/GetValidation.ts
+++ src/types/GetValidation.ts
@@ -1,6 +1,6 @@
 import {Translate} from 'react-admin';
 import * as Yup from 'yup';
 
-type GetValidation = (translate: Translate) => Yup.AnySchema;
+type GetValidation = (translate: Translate) => Yup.ObjectSchema<any, any, any, any>;
 
 export default GetValidation;
