1b406e8c82a70c956c77a451c27d83e8db4eefcd
Damir <tebuchet@gmail.com>
2022-07-07 15:53:50 +0300

    regexps

--- src/utils/regExps.ts
+++ src/utils/regExps.ts
@@ -1,5 +1,5 @@
 export const noDigitsRegExp = /^\D*$/gu;
 export const noSpaceRegExp = /^\S*$/gu;
-export const onlyLetters = /^[A-Za-zА-яа-я]+$/u;
+export const onlyLetters = /^[A-Za-zА-яа-яЁ-ё]+$/u;
 export const onlyDigits = /^\d+$/u;
 export const passwordRegExp = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/gu;
