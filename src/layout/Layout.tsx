fb62df8313f710866cfd5b7790495d8fa5764d7d
Damir <tebuchet@gmail.com>
2022-07-19 14:39:24 +0300

    deps

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import {Layout as RaLayout, LayoutProps} from 'react-admin';
+import {ReactQueryDevtools} from 'react-query/devtools';
 import AppBar from './AppBar';
 import Menu from './Menu';
 
-const Layout = (props: LayoutProps) => (<RaLayout {...props} appBar={AppBar} menu={Menu} />);
+const Layout = (props: LayoutProps) => (
+  <>
+    <RaLayout {...props} appBar={AppBar} menu={Menu} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </>
+);
 
 export default Layout;
