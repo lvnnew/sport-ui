6d90622e8b5d22247bdaf7463b15b244afb00f27
Damir <tebuchet@gmail.com>
2022-08-25 23:33:57 +0300

    gen

--- src/utils/regExps.ts
+++ src/utils/regExps.ts
@@ -1,6 +1,9 @@
 export const noDigitsRegExp = /^\D*$/gu;
 export const noSpaceRegExp = /^\S*$/gu;
 export const onlyLetters = /^[A-Za-zА-яа-яЁ-ё]+$/u;
+export const onlyLatinAndRussianLetters = /^[a-zа-яё\s]+$/iu;
 export const onlyLatinLetters = /^[A-Za-z]+$/u;
 export const onlyDigits = /^\d+$/u;
-export const passwordRegExp = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/gu;
+export const onlyPositiveDigits = /(?<![-.])\b[0-9]+\b(?!\.[0-9])/u;
+export const passwordRegExp = /(?=.*[0-9])(?=.*[!?@#$%^&*()_+=-><,./|])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!?@#$%^&*()_+=-><,./|]{6,}/gu;
+export const emailRegExp = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/gu;
