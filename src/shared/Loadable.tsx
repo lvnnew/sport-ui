da0d8c244c28bf208f07f51a783196744ce12f37
Damir <tebuchet@gmail.com>
2023-10-04 22:54:33 +0300

    loadable

--- src/shared/Loadable.tsx
+++ src/shared/Loadable.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import DefaultLoadable, {Options} from 'react-loadable';
-import Loader from './Loader';
-import {Optional} from 'utility-types';
+import React, {lazy, Suspense} from 'react';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-const Loadable = <Props, Exports extends object>(
-  options: Optional<Options<Props, Exports>, 'loading'>,
-) => DefaultLoadable({
-  ...options,
-  loading: () => <Loader />,
-} as Options<Props, Exports>);
+const Loadable = (factory: Parameters<typeof lazy>[0]) => () => {
+  const Component = lazy(factory);
+
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Component />
+    </Suspense>
+  );
+};
 
 export default Loadable;
