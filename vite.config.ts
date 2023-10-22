4b8677482359ce99fa64c202fc389d503491851d
Damir <tebuchet@gmail.com>
2023-10-22 15:20:07 +0300

    checks

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {checker} from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      enableBuild: true,
+    }),
+  ],
   server: {
     port: process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 8080,
   },
