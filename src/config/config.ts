231c515aada6938f0db5b5b5695d7dfaaf425c60
Damir <tebuchet@gmail.com>
2023-11-01 12:37:53 +0300

    oidc

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,14 @@
 const getConfig = async (): Promise<Record<string, string>> => {
+
   return fetch('/config.json')
     .then(data => data.json())
     .then(json => ({
       ...json,
-      adminRecaptchaPublicKey: import.meta.env.ADMIN_RECAPTCHA_PUBLIC_KEY || json.adminRecaptchaPublicKey,
+      adminRecaptchaPublicKey: import.meta.env.VITE_APP_ADMIN_RECAPTCHA_PUBLIC_KEY || json.adminRecaptchaPublicKey,
       endpoint: import.meta.env.VITE_APP_ENDPOINT || json.endpoint,
-      oidcAdmUrl: import.meta.env.OIDC_ADM_URL || json.oidcAdmUrl,
-      oidcAdmRealm: import.meta.env.OIDC_ADM_REALM || json.oidcAdmRealm,
-      oidcAdmClientId: import.meta.env.OIDC_ADM_CLIENT_ID || json.oidcAdmClientId,
+      oidcAdmUrl: import.meta.env.VITE_APP_OIDC_ADM_URL || json.oidcAdmUrl,
+      oidcAdmRealm: import.meta.env.VITE_APP_OIDC_ADM_REALM || json.oidcAdmRealm,
+      oidcAdmClientId: import.meta.env.VITE_APP_OIDC_ADM_CLIENT_ID || json.oidcAdmClientId,
     }));
 };
 
