9e55d4b155fc5f1eebd42d2687306ec1266dab10
leshenkov <lvn.new@gmail.com>
2024-04-15 20:43:32 +0300

    regen

--- src/adm/routes.tsx
+++ src/adm/routes.tsx
@@ -10,14 +10,14 @@ import additionalRoutes from './additionalRoutes';
 
 const LoadableDashboard = Loadable(() => import('./Dashboard'));
 const LoadableFunctions = Loadable(() => import('./functions/Functions'));
-const LoadableResourcesPage = Loadable(() => import('./ResourcesPage'));
+// const LoadableResourcesPage = Loadable(() => import('./ResourcesPage'));
 const LoadableMetaPage = Loadable(() => import('./MetaPage'));
 const LoadableDebugPage = Loadable(() => import('./utility/DebugPage'));
 
 export const routes = [
   <Route element={<Guard shouldHave='dashboard'><LoadableDashboard /></Guard>} key='dashboard' path='/dashboard' />,
   <Route element={<Guard shouldHave='functions'><LoadableFunctions /></Guard>} key='functions' path='/functions' />,
-  <Route element={<Guard shouldHave='resources'><LoadableResourcesPage /></Guard>} key='resources' path='/resources' />,
+  // <Route element={<Guard shouldHave='resources'><LoadableResourcesPage /></Guard>} key='resources' path='/resources' />,
   <Route element={<Guard shouldHave='meta'><LoadableMetaPage /></Guard>} key='meta' path='/meta' />,
   <Route element={<Guard shouldHave='debug'><LoadableDebugPage /></Guard>} key='debug' path='/debug' />,
 
